807c6366625c55a06d1d370141c0a07cd0d6b508
sbdywks <152150578+sbdywks@users.noreply.github.com>
2025-02-12 19:51:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'zho.ldym.xyz'
+      url.hostname = 'xjb.ldym.xyz'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
